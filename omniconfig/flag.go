bacf1b22be81f2d0b35a1a935bfe175c58862ef9
Jacob Miller <dev@jacobmiller22.com>
2025-09-29 21:49:56 -0400

    fix: access flag configurer

--- omniconfig/flag.go
+++ omniconfig/flag.go
@@ -6,13 +6,24 @@ type FlagConfigurerOption[T comparable] func(*FlagConfigurer[T])
 
 type FlagConfigurer[T comparable] struct {
 	Flagset *flag.FlagSet
+	Config  *T
 	Args    []string
 }
 
-func NewFlagConfigurer[T comparable](flagset *flag.FlagSet, opts ...FlagConfigurerOption[T]) *FlagConfigurer[T] {
-	return &FlagConfigurer[T]{
-		Flagset: flagset,
+func (fc *FlagConfigurer[T]) With(opts ...FlagConfigurerOption[T]) *FlagConfigurer[T] {
+	for _, opt := range opts {
+		opt(fc)
 	}
+	return fc
+}
+
+func NewFlagConfigurer[T comparable](flagset *flag.FlagSet, config *T, opts ...FlagConfigurerOption[T]) *FlagConfigurer[T] {
+	fc := &FlagConfigurer[T]{
+		Flagset: flagset,
+		Config:  config,
+	}
+
+	return fc.With(opts...)
 }
 
 func WithFlagConfigurerArgs[T comparable](args []string) FlagConfigurerOption[T] {
@@ -22,18 +33,8 @@ func WithFlagConfigurerArgs[T comparable](args []string) FlagConfigurerOption[T]
 }
 
 func (c FlagConfigurer[T]) Load() (*T, error) {
-	var cfg T
-
-	// c.flagset.IntVar(&cfg.Server.HTTP.Port, "server-http-port", 0, "Port to listen on for HTTP requests")
-	// c.flagset.StringVar(&cfg.Server.GRPC.Host, "server-grpc-host", "", "Hostname of the GRPC Server")
-	// c.flagset.IntVar(&cfg.Server.GRPC.Port, "server-grpc-port", 0, "Port to listen on for GRPC requests")
-	// c.flagset.StringVar(&cfg.DB.DSN, "db-dsn", "", "DSN to database")
-	// c.flagset.StringVar(&cfg.Log.Level, "log-level", "", "Log level to use")
-	// c.flagset.StringVar(&cfg.Log.File, "log-file", "", "Log file to use")
-
 	if err := c.Flagset.Parse(c.Args); err != nil {
 		return nil, err
 	}
-
-	return &cfg, nil
+	return c.Config, nil
 }
